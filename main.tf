0f95e6d9a3bc485ff7fb349d3d585222350e12fe
Allen <60677343+alienvera@users.noreply.github.com>
2023-12-21 10:01:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,6 +63,13 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "s3_versioning" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
